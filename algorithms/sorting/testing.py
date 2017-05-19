e0637b99fa3da32481ccdb3783ccd2be52ce8f40
calvarado <calvarado@wesleyan.edu>
2017-05-19 13:58:01 -0700

    Adding testing for quicksort algorithm.

---
+++ algorithms/sorting/testing.py
@@ -0,0 +1,7 @@
+import unittest
+import quicksort ###Not sure if this belongs here
+
+class QuickSortTests(unittest.TestCase):
+    """
+    These are several tests for the function quicksort.
+    """
